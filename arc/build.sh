669032e0166b127b3af63c2922017c827327567b
Sumit Kr Saha <sumitkr@gnosislab.in>
2022-02-08 17:58:15 +0530

    updated for command line

--- arc/build.sh
+++ arc/build.sh
@@ -3,11 +3,10 @@
 TAG=0.1
 REPO=gnosislab/arc
 
-CPU_BASE=ubuntu:20.04
-GPU_BASE=nvidia/cuda:11.2.0-cudnn8-runtime-ubuntu20.04
+echo "> CPU build"
+BASE=ubuntu:20.04
+docker build . -t $REPO:$TAG --build-arg BASE=$BASE
 
-echo ">>> CPU DOCKER"
-docker build . -t $REPO:$TAG --build-arg BASE=$CPU_BASE
-
-echo ">>> GPU DOCKER"
-docker build . -t $REPO:$TAG-gpu --build-arg BASE=$GPU_BASE
+echo "> GPU build"
+BASE=nvidia/cuda:11.2.0-cudnn8-runtime-ubuntu20.04
+docker build . -t $REPO:$TAG-gpu --build-arg BASE=$BASE
